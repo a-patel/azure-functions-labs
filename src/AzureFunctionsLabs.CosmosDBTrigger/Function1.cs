a9091385811799af70c35218c45993e793db2af4
Aashish Patel <toaashishpatel@gmail.com>
2019-05-11 14:34:43 +0530

    AzureFunctionsLabs.CosmosDBTrigger - added new function project

---
+++ src/AzureFunctionsLabs.CosmosDBTrigger/Function1.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsLabs.CosmosDBTrigger
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "databaseName",
+            collectionName: "collectionName",
+            ConnectionStringSetting = "CosmosDBConnectionString",
+            LeaseCollectionName = "leases")]IReadOnlyList<Document> input, ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                log.LogInformation("Documents modified " + input.Count);
+                log.LogInformation("First document Id " + input[0].Id);
+            }
+        }
+    }
+}
