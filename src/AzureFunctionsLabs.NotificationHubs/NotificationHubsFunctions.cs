27ebc0d0534674d3bc4316b1dca845bd72ad92ce
Aashish Patel <toaashishpatel@gmail.com>
2019-05-12 00:37:11 +0530

    Rename file

--- src/AzureFunctionsLabs.NotificationHubs/NotificationHubsFunctions.cs
+++ src/AzureFunctionsLabs.NotificationHubs/NotificationHubsFunctions.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 
 namespace AzureFunctionsLabs.NotificationHubs
 {
-    public static class Function1
+    public static class NotificationHubsFunctions
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
--- src/AzureFunctionsLabs.QueueTrigger/QueueFunctions.cs
+++ src/AzureFunctionsLabs.QueueTrigger/QueueFunctions.cs
@@ -1,16 +1,27 @@
+#region Imports
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging; 
+#endregion
 
 namespace AzureFunctionsLabs.QueueTrigger
 {
-    public static class Function1
+    public static class QueueFunctions
     {
-        [FunctionName("Function1")]
+        [FunctionName("QueueTrigger")]
         public static void Run([QueueTrigger("myqueue-items", Connection = "StorageConnectionString")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
     }
 }
+
+
+
+#region @@Reference
+/*
+
+ 
+*/
+#endregion
