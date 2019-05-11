91dae89d97c4779684a7248f5d0981751b06a638
Aashish Patel <toaashishpatel@gmail.com>
2019-05-12 00:45:29 +0530

    rename file

--- src/AzureFunctionsLabs.Twilio/TwilioFunctions.cs
+++ src/AzureFunctionsLabs.Twilio/TwilioFunctions.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 
 namespace AzureFunctionsLabs.Twilio
 {
-    public static class Function1
+    public static class TwilioFunctions
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
