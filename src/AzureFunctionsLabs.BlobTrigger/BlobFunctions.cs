24d2f0292467dc910af361926d5a6fcd3c42079a
Ashish Patel <toaashishpatel@gmail.com>
2020-02-18 00:32:50 +0530

    GenerateThumb - using ImageResizer

--- src/AzureFunctionsLabs.BlobTrigger/BlobFunctions.cs
+++ src/AzureFunctionsLabs.BlobTrigger/BlobFunctions.cs
@@ -3,6 +3,7 @@ using AzureFunctionsLabs.BlobTrigger.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using ImageResizer;
 #endregion
 
 namespace AzureFunctionsLabs.BlobTrigger
@@ -28,11 +29,30 @@ namespace AzureFunctionsLabs.BlobTrigger
 
 
         [FunctionName("BlogTrigger")]
-        public static void Run([BlobTrigger("media-files/{name}", Connection = "StorageConnectionString")]Stream myBlob, string name, ILogger log)
+        public void Run([BlobTrigger("media-files/{name}", Connection = "StorageConnectionString")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
 
+
+        [FunctionName("GenerateThumb")]
+        public void GenerateThumb(
+            [BlobTrigger("originals/{name}", Connection = "StorageConnectionString")]Stream image,
+            [Blob("thumbs/s-{name}", FileAccess.Write, Connection = "StorageConnectionString")]Stream imageSmall,
+            ILogger log)
+        {
+            var instructions = new Instructions
+            {
+                Height = 320,
+                Width = 200,
+                Mode = FitMode.Carve,
+                Scale = ScaleMode.Both
+            };
+
+            ImageBuilder.Current.Build(new ImageJob(image, imageSmall, instructions));
+
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{image} \n Size: {image.Length} Bytes");
+        }
         #endregion
 
         #region Utilities
@@ -40,3 +60,12 @@ namespace AzureFunctionsLabs.BlobTrigger
         #endregion
     }
 }
+
+
+
+#region @@Reference
+/*
+https://microsoft.github.io/AzureTipsAndTricks/blog/tip158.html
+ 
+*/
+#endregion
